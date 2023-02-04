06546f00666960f857844bf13db100d4c65480d1
HoseanRC <hoseandawoodi90@outlook.com>
2023-02-04 12:39:35 +0330

    added sorting by country emoji

--- index.js
+++ index.js
@@ -34,36 +34,40 @@ async function getNodes() {
 	var nodes_str = await getNodes();
 	var hosts_array = [];
 	var nodes_array = nodes_str.split("\n");
+	var nodes_output_array = [];
 	var nodes_output = "";
 	console.log(`${nodes_array.length} nodes in total...`);
 	for(var i = 0; i < nodes_array.length; i++){
 		var node = nodes_array[i];
 		let hostname = "";
+		let flag = "";
+		let link = "";
 		switch(node.split("://")[0]){
 			case "ss":
 			case "trojan":
-				let a = node.substring(
-					node.indexOf("@") + 1, 
-					node.length
-				);
-				hostname = a.substring(
-					0,
-					a.indexOf(":")
-				);
+				hostname = /(?:.*@)(.*?)[:#]/g.exec(node)[1];
+				flag = Buffer.from(/#.{3}(.{24})/.exec(node)[1].replaceAll("%", ""), "hex").toString();
 				break;
 			case "ssr":
-				hostname = Buffer.from(node.split("://")[1], "base64").toString().split(":")[0];
+				link = Buffer.from(node.split("://")[1], "base64").toString();
+				hostname = link.split(":")[0];
+				flag = /\[(.{4})\]/g.exec(Buffer.from(/(?:.*remarks=)(.*?)(&|$)/g.exec(link)[1],"base64").toString())[1];
 				break;
 			case "vmess":
-				hostname = JSON.parse(Buffer.from(node.split("://")[1], "base64").toString()).host;
+				link = JSON.parse(Buffer.from(node.split("://")[1], "base64").toString());
+				hostname = link.host;
+				flag = /\[(.{4})\]/g.exec(link.ps)[1];
 				break;
 		}
 		if(hostname != "" &! hosts_array.includes(hostname)){
 			hosts_array.push(hostname);
-			nodes_output += node;
-			nodes_output += "\n";
+			nodes_output_array.push(flag + node);
 		}
 	}
+	nodes_output_array.sort().forEach(node => {
+		nodes_output += /(?:.{4})(.*)/.exec(node)[1];
+		nodes_output += "\n";
+	});
 	fs.writeFile('nodes.txt', nodes_output, err => {
 		console.log(`\n${nodes_output.split("\n").length-1} unique nodes!`);
 		if (err) {
